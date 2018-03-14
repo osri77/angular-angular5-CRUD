46ed50ec6f675b68f97b10fb6600b4458ff7a6e3
Srinivas Oruganti <SrinivasOruganti@ecq.local>
2018-03-14 15:02:49 +1000

    Updated the angular form by adding radio buttons, checkboxes, dropdown list and datepicker from bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {FormsModule} from '@angular/forms';
+import {BsDatepickerModule} from 'ngx-bootstrap/datepicker';
 
 
 import { AppComponent } from './app.component';
@@ -24,6 +25,7 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
+    BsDatepickerModule.forRoot(),
     RouterModule.forRoot(appRoutes)
   ],
   providers: [],
--- src/app/employees/create-employee.component.ts
+++ src/app/employees/create-employee.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import {Department} from '../models/department.model'
+
 
 @Component({
   selector: 'app-create-employee',
@@ -12,6 +14,19 @@ export class CreateEmployeeComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  // gender:string = "female";
+  //contactPreference='email';
+  //isActive = 'true';
+  //department = "3";
+
+  departments:Department[] = [
+    { id: 1, name: 'Help Desk' },
+    { id: 2, name: 'HR' },
+    { id: 3, name: 'IT' },
+    { id: 4, name: 'Payroll' }
+  ];
+  
 
   saveEmployee(employeeForm: NgForm):void {
     console.log(employeeForm.value);
